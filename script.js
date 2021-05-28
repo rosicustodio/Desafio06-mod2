df71663ea364c1c50ac5d9bd1fbee0b9650aeabd
rosicustodio <rosimarcustodio@gmail.com>
2021-05-28 16:55:26 -0300

    desafio 6

---
+++ script.js
@@ -0,0 +1,62 @@
+let sobreMim = {
+    name:"Rosi Custódio",
+    cabelo:"preto",
+    olhos:"castanho escuro"
+}
+
+
+//delete sobreMim.olhos
+
+sobreMim.profissao="confeiteira"
+
+console.log(sobreMim)
+
+
+let cadastro = [
+{
+    nome: `Paula`,
+    idade:35,
+    telefone:99998-8888,
+    amigos:[`Carol`,`Hanna`,`Carlos`,`Andréia`,`Patrícia`]
+},
+
+{
+    nome:`Simara`,
+    idade:25,
+    telefone:95555-4444,
+    amigos:[`Thaís`,`Camila`,`Sandra`,`Peter`,`Rodrigo`]
+},
+
+{
+    nome: `Letícia`,
+    idade:27,
+    telefone:90001-4333,
+    amigos:[`Carla`,`Sara`,`Rafaela`,`Robson`,`Rúbia`]
+
+},
+
+{
+    nome:`Marcos`,
+    idade:31,
+    telefone:92323-2323,
+    amigos:[`Kátia`,`Sônia`,`Adelson`,`Jhony`,`Gisele`]
+},
+
+{
+    nome:`Monique`,
+    idade:40,
+    telefone:91010-3030,
+    amigos:[`Saul`,`Gilson`,`Suzana`,`Maria`,`Josy`]
+},
+
+]
+
+    console.log(cadastro[0].amigos[1])
+
+    console.log(cadastro[1].amigos[2])
+
+    console.log(cadastro[2].amigos[3])
+
+    console.log(cadastro[3].amigos[4])
+
+    console.log(cadastro[4].amigos[0])
